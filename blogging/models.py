c4fbbb030fda9397a6d1dd8d38d436f616052924
Brad Gross <bradley.n.gross@outlook.com>
2023-05-07 13:26:21 -0700

    In the blogging model, added Category class and migrated it to the database and added it to the admin

--- blogging/models.py
+++ blogging/models.py
@@ -13,3 +13,8 @@ class Post(models.Model):
         return self.title
     
 
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    posts = models.ManyToManyField(Post, blank=True, related_name='categories')
+

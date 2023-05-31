b44a4f65593e2963214d9b96016af3bc26e3eccc
Brad Gross <bradley.n.gross@outlook.com>
2023-05-30 21:01:14 -0700

    Finished Assignment 8 by converting the function-based views in the blogging app into class-based views

--- blogging/views.py
+++ blogging/views.py
@@ -1,21 +1,21 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)
+class BloggingListView(ListView):
+    template_name = 'blogging/list.html'
+
+    def get_queryset(self):
+        self.published = Post.objects.exclude(published_date__exact=None)
+        return self.published.order_by('-published_date')
 
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+class BloggingDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
